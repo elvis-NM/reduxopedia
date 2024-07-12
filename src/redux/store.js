8c1d4845e605e2e19777df32df42ee73e186f564
Elvis Makia <elvis_makia@yahoo.com>
2024-07-11 22:51:20 -0400

    added destination slice ,store reducer   commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./slice/counterSlice";
+import { destinationReducer } from "./slice/destinationSlice";
 
 export const store = configureStore({
   reducer: {
     counterStore: counterReducer,
+    destinationStore: destinationReducer,
     //we will fill this reducer in the upcoming videos
   },
 });
 
-console.log(store.getState());
-store.dispatch({
-  type: "counter/increment",
-});
+// console.log(store.getState());
+// store.dispatch({
+//   type: "counter/increment",
+// });
 
 console.log(store.getState());
