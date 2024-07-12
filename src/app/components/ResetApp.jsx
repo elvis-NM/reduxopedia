6a3f6fc6a8fde6f5f56289a599c77dc7e1e6c16b
Elvis Makia <elvis_makia@yahoo.com>
2024-07-12 13:57:25 -0400

    added destination component and use state  commit

---
+++ src/app/components/ResetApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ResetApp() {
+  return (
+    <div className="text-center">
+      <button className="btn btn-warning">Reset App</button>
+    </div>
+  );
+}
+
+export default ResetApp;
